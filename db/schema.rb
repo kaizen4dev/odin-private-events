0637fcdbb26f3a7eabbb94a7f3649f36630220bd
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-11-07 22:33:22 +0000

    Add name to the event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_07_215644) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_07_222821) do
   create_table "events", force: :cascade do |t|
     t.string "location"
     t.string "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 end
