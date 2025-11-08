c48ad3f793166a0833e686de3c359d65465c44a5
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-11-08 23:45:28 +0000

    Comply with rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, path: ""
 
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
-    get 'sign_out', to: 'devise/sessions#destroy'
+    get "sign_in", to: "devise/sessions#new"
+    get "sign_out", to: "devise/sessions#destroy"
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :events
-  resources :users, only: [:show]
+  resources :users, only: [ :show ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
