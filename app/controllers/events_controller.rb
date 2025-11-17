1ac727c3bcd5f999717631303657232d73eff148
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-11-17 23:29:27 +0000

    EventsController: add #new and #create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build event_params
+
+    if @event.save
+      redirect_to user_path current_user.id
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.expect event: [:name, :time, :location]
+  end
 end
