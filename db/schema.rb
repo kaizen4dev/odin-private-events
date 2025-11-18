cac34c4149f79060fa696f2fb38c3d46e51ea041
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-11-18 22:58:32 +0000

    Run database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_08_192253) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_18_225011) do
+  create_table "event_attendings", force: :cascade do |t|
+    t.integer "attendee_id", null: false
+    t.integer "atteneded_event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attendee_id"], name: "index_event_attendings_on_attendee_id"
+    t.index ["atteneded_event_id"], name: "index_event_attendings_on_atteneded_event_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "location"
     t.string "time"
@@ -33,5 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_08_192253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "event_attendings", "attendees"
+  add_foreign_key "event_attendings", "atteneded_events"
   add_foreign_key "events", "users", column: "creator_id"
 end
