de7acb2caedf3df774f9b5f53d344dfff03e3b71
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-11-19 23:50:58 +0000

    Add uniqueness validation for event attendings

--- app/models/event_attending.rb
+++ app/models/event_attending.rb
@@ -1,4 +1,6 @@
 class EventAttending < ApplicationRecord
   belongs_to :attendee, class_name: "User"
   belongs_to :attended_event, class_name: "Event"
+
+  validates :attendee_id, uniqueness: { message: "already joined this event", scope: [:attended_event_id] }
 end
