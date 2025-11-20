d2aabb6a0df912af7031fed4efccad0252efd833
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-11-20 03:11:09 +0000

    Add create and destroy methods fore EventAttendingsController

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,2 +1,15 @@
 class EventAttendingsController < ApplicationController
+  def create
+    @event_attending = EventAttending.build attendee_id: params[:user_id], attended_event_id: params[:event_id]
+
+    if @event_attending.save
+      redirect_back fallback_location: root_path, notice: "You joined the event!"
+    end
+  end
+
+  def destroy
+    @event_attending = EventAttending.find [params[:user_id], params[:event_id]]
+    @event_attending.destroy
+    redirect_back fallback_location: root_path, notice: "You left the event."
+  end
 end
