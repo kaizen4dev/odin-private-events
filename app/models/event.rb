587b61a86ddaa71f32a3ca0a0aa3cac696958f9f
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-11-21 18:58:10 +0200

    Use default scope to sort Events by time

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :event_attendings, foreign_key: "attended_event_id"
   has_many :attendees, through: :event_attendings
+
+  default_scope {order :time}
 end
